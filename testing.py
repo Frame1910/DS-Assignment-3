2b65fbaca4883a82c0dc93da05b552bd9348225d
Darren Meiring <dpmeiring@gmail.com>
2021-05-24 17:21:51 +0800

    Data for unit testing

---
+++ testing.py
@@ -0,0 +1,19 @@
+# ! Mock data variables
+mock_unit_list = [
+    ["CSP0000", 40],
+    ["CSP0001", 40],
+    ["CSP0002", 40],
+    ["CSP0003", 40],
+    ["CSP0004", 40],
+    ["CSP0005", 40],
+    ["CSP0006", 40],
+    ["CSP0007", 50],
+    ["CSP0008", 50],
+    ["CSP0009", 50],
+    ["CSP0010", 50],
+    ["CSP0011", 50]
+]
+mock_person = [
+    "10496696",
+    mock_unit_list
+]
