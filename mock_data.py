2b56d75d3810c508de92d2707ad82c38fd32b67f
Byron Gregoriadis <byron.gregoriadis@gmail.com>
2021-05-26 15:55:22 +0800

    Created mock_data file with 7 "students"

---
+++ mock_data.py
@@ -0,0 +1,132 @@
+#STUDENT 1 | QUALIFIED FOR HONOURS STUDY!
+student1 = [
+    ["SCI1125", 96],
+    ["CSP1150", 92],
+    ["MAT1252", 92],
+    ["CSI1241", 95],
+    ["CSG1105", 86],
+    ["CSI1101", 78],
+    ["ENS1161", 94],
+    ["CSG1207", 91],
+    ["CSP2348", 86],
+    ["CSP2104", 88],
+    ["CSG2341", 90],
+    ["CSG2344", 86],
+    ["CSI3344", 76],
+    ["CSP3341", 78],
+    ["CSP2108", 75],
+    ["CSI2312", 72]]
+
+#STUDENT 2 | MAY HAVE GOOD CHANCE! Need further assessment!
+student2 = [
+    ["SCI1125", 82],
+    ["CSP1150", 78],
+    ["MAT1252", 80],
+    ["CSI1241", 85],
+    ["CSG1105", 75],
+    ["CSI1101", 70],
+    ["ENS1161", 90],
+    ["CSG1207", 87],
+    ["CSP2348", 80],
+    ["CSP2104", 77],
+    ["CSG2341", 82],
+    ["CSG2344", 80],
+    ["CSI3344", 35],
+    ["CSP3341", 40],
+    ["CSP2108", 27],
+    ["CSI2312", 40]]
+
+#STUDENT 3 | MAY HAVE A CHANCE! Must be carefully reassessed and get the coordinator’s special permission!
+student3 = [
+    ["SCI1125", 73],
+    ["CSP1150", 73],
+    ["MAT1252", 76],
+    ["CSI1241", 75],
+    ["CSG1105", 72],
+    ["CSI1101", 72],
+    ["ENS1161", 81],
+    ["CSG1207", 76],
+    ["CSP2348", 74],
+    ["CSP2104", 71],
+    ["CSG2341", 67],
+    ["CSG2344", 70],
+    ["CSI3344", 58],
+    ["CSP3341", 57],
+    ["CSP2108", 62],
+    ["CSI2312", 52]]
+
+#STUDENT 4 | DOES NOT QUALIFY FOR HONORS STUDY! Try Masters by course work.
+student4 = [
+    ["SCI1125", 64],
+    ["CSP1150", 54],
+    ["MAT1252", 78],
+    ["CSI1241", 43],
+    ["CSG1105", 65],
+    ["CSI1101", 70],
+    ["ENS1161", 64],
+    ["CSG1207", 64],
+    ["CSP2348", 57],
+    ["CSP2104", 81],
+    ["CSG2341", 79],
+    ["CSG2344", 67],
+    ["CSI3344", 54],
+    ["CSP3341", 55],
+    ["CSP2108", 66],
+    ["CSI2312", 71]]
+
+#STUDENT 5 | You've failed", unit_result[0], "three times, you do not qualify for Honours.
+student5 = [
+    ["SCI1125", 82],
+    ["CSP1150", 78],
+    ["MAT1252", 80],
+    ["CSI1241", 85],
+    ["CSG1105", 75],
+    ["CSI1101", 70],
+    ["ENS1161", 90],
+    ["CSG1207", 87],
+    ["CSP2348", 80],
+    ["CSP2104", 77],
+    ["CSG2341", 82],
+    ["CSG2344", 80],
+    ["CSP3341", 40],
+    ["CSI2312", 35],
+    ["CSI2312", 27],
+    ["CSI2312", 40]]
+
+#STUDENT 6 | You cannot pass one unit more than once.
+student6 = [
+    ["SCI1125", 82],
+    ["CSP1150", 78],
+    ["MAT1252", 80],
+    ["CSI1241", 85],
+    ["CSG1105", 75],
+    ["CSI1101", 70],
+    ["ENS1161", 90],
+    ["CSG1207", 87],
+    ["CSP2348", 80],
+    ["CSP2104", 77],
+    ["CSG2341", 82],
+    ["CSG2344", 80],
+    ["CSP3341", 40],
+    ["CSI2312", 35],
+    ["CSI2312", 57],
+    ["CSI2312", 61]]
+
+#STUDENT 7 | You have failed 6 or more of your units, you do not qualify for Honours.
+student7 = [
+    ["SCI1125", 82],
+    ["CSP1150", 78],
+    ["MAT1252", 80],
+    ["CSI1241", 85],
+    ["CSG1105", 30],
+    ["CSI1101", 70],
+    ["ENS1161", 29],
+    ["CSG1207", 87],
+    ["CSP2348", 80],
+    ["CSP2104", 37],
+    ["CSG2341", 82],
+    ["CSG2344", 80],
+    ["CSP3341", 40],
+    ["CSI2312", 35],
+    ["CSI2312", 27],
+    ["CSI2312", 50]]
