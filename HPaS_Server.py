d414731cca7174374bdfdbada51e331b2cced9a1
Darren Meiring <dpmeiring@gmail.com>
2021-05-28 23:33:17 +0800

    Added new developer area - test honours

--- HPaS_Server.py
+++ HPaS_Server.py
@@ -203,6 +203,88 @@ with SimpleXMLRPCServer(('localhost', 8000), requestHandler=RequestHandler) as s
             elif choice == "del":
                 param = input("ID: ")
                 db.deleteStudentData(param)
+            elif choice == "test honours":
+                while True:
+                    level = input(
+                        "Pick level: Qualified, May1, May2, Not OR exit? ")
+                    if level == "exit":
+                        break
+                    elif level == "Qualified":
+                        person = ["10496696", [
+                            ["SCI1125", 96],
+                            ["CSP1150", 92],
+                            ["MAT1252", 92],
+                            ["CSI1241", 95],
+                            ["CSG1105", 86],
+                            ["CSI1101", 78],
+                            ["ENS1161", 94],
+                            ["CSG1207", 91],
+                            ["CSP2348", 86],
+                            ["CSP2104", 88],
+                            ["CSG2341", 90],
+                            ["CSG2344", 86],
+                            ["CSI3344", 76],
+                            ["CSP3341", 78],
+                            ["CSP2108", 75],
+                            ["CSI2312", 72]]]
+                        print(determineHonours(person))
+                    elif level == "May1":
+                        person = ["10496696", [
+                            ["SCI1125", 82],
+                            ["CSP1150", 78],
+                            ["MAT1252", 80],
+                            ["CSI1241", 85],
+                            ["CSG1105", 75],
+                            ["CSI1101", 70],
+                            ["ENS1161", 90],
+                            ["CSG1207", 87],
+                            ["CSP2348", 80],
+                            ["CSP2104", 77],
+                            ["CSG2341", 82],
+                            ["CSG2344", 80],
+                            ["CSI3344", 35],
+                            ["CSP3341", 40],
+                            ["CSP2108", 27],
+                            ["CSI2312", 40]]]
+                        print(determineHonours(person))
+                    elif level == "May2":
+                        person = ["10496696", [
+                            ["SCI1125", 73],
+                            ["CSP1150", 73],
+                            ["MAT1252", 76],
+                            ["CSI1241", 75],
+                            ["CSG1105", 72],
+                            ["CSI1101", 72],
+                            ["ENS1161", 81],
+                            ["CSG1207", 76],
+                            ["CSP2348", 74],
+                            ["CSP2104", 71],
+                            ["CSG2341", 67],
+                            ["CSG2344", 70],
+                            ["CSI3344", 58],
+                            ["CSP3341", 57],
+                            ["CSP2108", 62],
+                            ["CSI2312", 52]]]
+                        print(determineHonours(person))
+                    elif level == "Not":
+                        person = ["10496696", [
+                            ["SCI1125", 64],
+                            ["CSP1150", 54],
+                            ["MAT1252", 78],
+                            ["CSI1241", 43],
+                            ["CSG1105", 65],
+                            ["CSI1101", 70],
+                            ["ENS1161", 64],
+                            ["CSG1207", 64],
+                            ["CSP2348", 57],
+                            ["CSP2104", 81],
+                            ["CSG2341", 79],
+                            ["CSG2344", 67],
+                            ["CSI3344", 54],
+                            ["CSP3341", 55],
+                            ["CSP2108", 66],
+                            ["CSI2312", 71]]]
+                        print(determineHonours(person))
 
     # ! End developer area =======================================================
     else:
